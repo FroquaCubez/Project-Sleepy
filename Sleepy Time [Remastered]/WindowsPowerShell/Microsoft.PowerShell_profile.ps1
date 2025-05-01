e578ce4160c81aefef241cfb6adc4723cf7656a6
FroquaCubez <froquacubez@gmail.com>
2025-05-01 12:31:47 +0800

    Upload 1

---
+++ Sleepy Time [Remastered]/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,19 @@
+$Env:KOMOREBI_CONFIG_HOME = 'C:\Users\Aaron\.config\komorebi'
+
+$PSVersionTable.PSEdition -ne 'Core' | Out-Null
+
+
+oh-my-posh init pwsh --config 'C:\Users\Aaron\.config\ohmyposh\zen.toml' | Invoke-Expression
+
+# terminal icons
+Import-Module -Name Terminal-Icons
+
+# Shows navigable menu of all options when hitting Tab
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+# Autocompletion for arrow keys
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+# aliases
+Set-Alias -Name c -Value clear
